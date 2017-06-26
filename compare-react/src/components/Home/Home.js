f2ebc330ab9f4d5b24b4034e7dd0568e52dbd7a9
Nirmal Kumar V <vnknirmalkumar@gmail.com>
2017-06-26 20:49:19 +0800

    compare react done

--- compare-react/src/components/Home/Home.js
+++ compare-react/src/components/Home/Home.js
@@ -1,10 +1,78 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './Home.css'
 class Home extends Component {
-  render () {
+
+  render() {
+
+    let heading = 'Digital Innovation Master';
+    let subheading = 'However it was a lion. Gluten. Not just my sorrow, carrots easy to put passenger' +
+        's. For, not to the supreme ends of. The refinancing. Clinical it is now. Volleyb' +
+        'all court football macro developer need Vulputate';
+
     return (
-      <div>
-        Home
+      <div className="home">
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">{heading}</h1>
+              <div className="is-two-thirds column is-paddingless">
+                <h2 className="subtile is-4">{subheading}</h2>
+              </div>
+              <a href="" className="button is-primary" id="learn">Read More</a>
+            </div>
+          </div>
+        </section>
+
+        <section className="section">
+          <div className="container">
+            <div className="columns pd is-desktop">
+              <div className="column is-1 has-text-centered">
+                <i className="fa fa-bolt is-primary"></i>
+              </div>
+              <div className="column is-one-third-desktop">
+                <p className="title">
+                  <strong>We provider quality product services to grow your bussiness with low cost</strong>
+                </p>
+              </div>
+              <div className="column">
+                <p>
+                  <strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at
+                    suscipit purus.
+                  </strong>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="columns pd">
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <p className="title">“We are growing as fast with poomugam product services.
+                    Quality at its first place.”</p>
+                  <p className="subtitle">- Gary Simon</p>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <p className="title">“We are growing as fast with poomugam product services.
+                    Quality at its first place.”</p>
+                  <p className="subtitle">- Gary Simon</p>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <p className="title">“We are growing as fast with poomugam product services.
+                    Quality at its first place.”</p>
+                  <p className="subtitle">- Gary Simon</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     )
   }
--- compare-react/src/components/Products/Products.js
+++ compare-react/src/components/Products/Products.js
@@ -1,11 +1,58 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import axios from 'axios';
 import './Products.css';
 
 class Products extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      errors: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        const products = response
+          .data
+          .slice(0, 10);
+        this.setState({products})
+      })
+      .catch(e => {
+        const errors = [];
+        errors.push(e);
+        this.setState({errors})
+      })
+  }
+  render() {
     return (
-      <div>
-        Products
+      <div className="products">
+        <div className="container">
+          <div className="section">
+            <h1 className="title">test</h1>
+            <h2 className="subtitle is-4">test</h2>
+            <div className="columns">
+              {this
+                .state
+                .products
+                .map(product => {
+                  return (
+                    <div className="column is-one-third" key={product.id}>
+                      <div className="card">
+                        <div className="card-content">
+                          <p className="title">{product.title}</p>
+                          <p className="answer">{product.body}</p>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
